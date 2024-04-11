7cda7505b9c6676adbd52c1467630dd41e1479d5
Ulukbek <l9l9ndos@gmail.com>
2024-04-11 14:54:10 +0600

    Chats

--- apps/chat_contacts/urls.py
+++ apps/chat_contacts/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import ChatContactListAPIView
 
 urlpatterns = [
-    path("choice_chat/", ChatContactListAPIView.as_view()),
+    path("chats/", ChatContactListAPIView.as_view()),
 ]
